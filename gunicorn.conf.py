13adf1bb5b889174cc699b38a6488a36a5509365
Giacomo Mandanici <giacomo.mandanici@liparipeople.com>
2026-06-22 10:14:48 +0200

    DAY10: ADD: exercise 2 - gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,16 +1,13 @@
 import multiprocessing
-import os
 
 wsgi_app = "app.main:app"
-bind = os.getenv("GUNICORN_BIND", "0.0.0.0:8000")
-workers = int(os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1))
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
-timeout = int(os.getenv("GUNICORN_TIMEOUT", 120))
-graceful_timeout = int(os.getenv("GUNICORN_GRACEFUL_TIMEOUT", 30))
-keepalive = int(os.getenv("GUNICORN_KEEPALIVE", 5))
-max_requests = int(os.getenv("GUNICORN_MAX_REQUESTS", 1000))
-max_requests_jitter = int(os.getenv("GUNICORN_MAX_REQUESTS_JITTER", 50))
+timeout = 60
+graceful_timeout = 30
+keepalive = 5
 
-accesslog = os.getenv("GUNICORN_ACCESS_LOG", "-")
-errorlog = os.getenv("GUNICORN_ERROR_LOG", "-")
-loglevel = os.getenv("GUNICORN_LOG_LEVEL", "info")
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
